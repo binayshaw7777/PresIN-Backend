eb9adfbcb3d5f8884faa89020cad7ae1cb7847fe
Binay Shaw <binayshaw7777@gmail.com>
2023-03-19 14:23:34 +0530

    Completed Attendance entity

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -5,12 +5,75 @@ const SORT_ORDER_DESC = "desc";
 
 class AttendanceController {
 
+    static allAttendees = async (req, res) => {
+      try {
+        const filter = {};
+        const sort = {};
+
+        // Extract query parameters and add to filter object
+        const queryParams = Object.keys(req.query);
+        queryParams.forEach((param) => {
+          if (param === SORT_BY) {
+            const sortOrder = req.query.sortOrder === SORT_ORDER_DESC ? -1 : 1;
+            sort[req.query.sortBy] = sortOrder;
+          } else {
+            filter[param] = req.query[param];
+          }
+        });
+
+        //Check if list of all attendees is empty or not
+        const listOfAllAttendees = await Attendance.find(filter).sort(sort);
+        if (!listOfAllAttendees || listOfAllAttendees.length == 0)
+          return res.send({ status: 400, message: "No attendees found!" });
+
+        res
+          .status(200)
+          .send({
+            status: 200,
+            message: "All attendees fetched successfully!",
+            attendanceList: listOfAllAttendees,
+          });
+
+      } catch (error) {
+        console.log(error);
+        res
+          .status(400)
+          .send({ status: 400, message: `Something went wrong! ${error}` });
+      }
+    }
+
     static createAttendance = async (req, res) => {
         try {
-            const {userID, name, markedMinute, markedHour, markedDate, markedMonth, markedYear, organization, location, faceEmbeddings, matchPercentage} = req.body;
+            const {
+              userID,
+              name,
+              markedMinute,
+              markedHour,
+              markedDate,
+              markedMonth,
+              markedYear,
+              organization,
+              location,
+              faceEmbeddings,
+              matchPercentage,
+            } = req.body;
 
-            if (!userID || !name || !markedMinute || !markedHour || !markedDate || !markedMonth || !markedYear || !organization || !location || !faceEmbeddings || !matchPercentage) {
-                return res.status(400).send({status: 400, message: "All fields are required!"});
+            if (
+              !userID ||
+              !name ||
+              !markedMinute ||
+              !markedHour ||
+              !markedDate ||
+              !markedMonth ||
+              !markedYear ||
+              !organization ||
+              !location ||
+              !faceEmbeddings ||
+              !matchPercentage
+            ) {
+              return res
+                .status(400)
+                .send({ status: 400, message: "All fields are required!" });
             }
 
             const existingAttendance = await Attendance.findOne({
@@ -46,6 +109,36 @@ class AttendanceController {
             console.log(error);
             res.status(400).send({ status: 400, message: `Something went wrong! ${error}`});
         }
+    }
+
+
+    static getAttendanceById = async (req, res) => {
+      try {
+        const attendanceIsPresent = await Attendance.findById(req.params.id);
+        if (!attendanceIsPresent) {
+          return res
+            .status(404)
+            .send({ status: 404, message: "Attendance not found!" });
+        }
+        res
+          .status(200)
+          .send({
+            status: 200,
+            message: "Attendance fetched successfully!",
+            data: attendanceIsPresent,
+          });
+      } catch (error) {
+        console.error(error);
+        if (error.name === "CastError") {
+          return res
+            .status(400)
+            .send({
+              status: 400,
+              message: `Invalid ${error.path}: ${error.value}`,
+            });
+        }
+        res.status(500).send({ status: 500, message: "Something went wrong!" });
+      }
     }
 
 }
